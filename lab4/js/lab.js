cb909a4c7c66ec4ea73cf92535a35609c9e90bd0
senava <78111849+senava@users.noreply.github.com>
2022-02-08 20:54:18 -0800

    lab 4 frameworl
    02/08

---
+++ lab4/js/lab.js
@@ -0,0 +1,25 @@
+// Summary. This file contains algorithms for an everyday task and a computer task
+// Description. This file contains algorithms for a making brownies and playing Rock, Paper, Scissors
+// @link   https://reckhardt.github.io/cst252/lab4/index.html
+// @link
+// @file   This files contains psuedocode
+// @author Robert Eckhardt & Stephanie Nava
+// @since  02.08.22
+
+// Pseudocode of Everyday Task: Baking Brownies
+    // Gather the following ingredients: ½ cup butter, 1 cup white sugar, 2 eggs, 1 teaspoon vanilla extract, ⅓ cup unsweetened cocoa powder, ½ cup all-purpose flour, ¼ teaspoon salt, ¼ teaspoon baking powder
+    // Preheat oven to 350 degrees F
+    // Grease and flour an 8 inch square pan, set aside for later use
+    // Grab a large saucepan mel ½ cup of butter
+    // Once melted, remove from heat and stir in sugar, eggs, and 1 teaspoon vanilla.
+    // Once everything is stirred in, beat in ⅓ cup of cocoa powder, ½ cup of flour, salt, and baking powder.
+    // When done, pour the batter into the prepared pan and spread it evenly around.
+    // Place in preheated oven and bake for 25-30 min.
+
+// Pseudocode of Computer Task: Playing Rock, Paper, Scissors
+    // While there is not a winner
+        // Prompt the user to choose either, Rock, Paper, or Scissors
+        // Randomly assign the computer Rock, Paper, or Scissors
+        // Use if/else to determine winner
+        // If there is no winner return to beginning of program
+    // Display who won and end program
